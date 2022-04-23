7dc7550817d4d4a62c01ad99fcf98a319f3a1053
Raja Vignesh <91282922+rajavigneshselvamani@users.noreply.github.com>
2022-04-22 21:24:01 -0300

    Update main.py
    Refer the document 'Google Drive mount' and run the main.py code

--- main.py
+++ main.py
@@ -2,21 +2,27 @@ import json
 import requests
 import pandas as pd
 import numpy as np
+#from sklearn import metrics
 from sklearn.cluster import Birch
+#from sklearn.cluster import KMeans
+#from sklearn.metrics import calinski_harabasz_score
 from sklearn.metrics import silhouette_score
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import MinMaxScaler
-
+#from sklearn.cluster import MeanShift
+# from sklearn.preprocessing import StandardScaler
+# from sklearn.decomposition import IncrementalPCA
+#from sklearn.cluster import AgglomerativeClustering
 
 #Data File read
-gene_data1 = pd.read_csv(r"/content/drive/MyDrive/Clustering_competition_1/New_data_trV1.csv", header=None)
-gene_test=pd.read_table(r'/content/drive/MyDrive/Bio_Project/data_ts.txt', header=None)
+gene_train = pd.read_table(r"/content/drive/MyDrive/Clustering_competition/data_tr.txt", header=None)
+gene_test=pd.read_table(r'/content/drive/MyDrive/Clustering_competition/data_ts.txt', header=None)
 
 #MinMaxScalar :
 np.random.seed(0)
 scaler = MinMaxScaler(feature_range=(0, 2))
-scaler.fit(gene_data1)
-gene_data2 = scaler.transform(gene_data1)
+scaler.fit(gene_train)
+gene_data2 = scaler.transform(gene_train)
 minmax = scaler.transform(gene_test)
 
 #PCA :
@@ -45,4 +51,4 @@ response = requests.request("POST", url, headers=headers, data=payload_MShift)
 print('=======Result==========')
 print('Birch Accuracy Result: %', response.text)
 print('Birch Silhouette Score: %.3f' % BSil_score)
-
+#print('Birch Calinski_haraba Score: %.3f' % CHS)
